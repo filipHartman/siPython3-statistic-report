fe6a5936e68628da034c688b4f521bdc24e49180
filipHartman <filip.hartman23@gmail.com>
2017-08-29 11:51:52 +0200

    Defining mandatory functions in report.py

---
+++ reports.py
@@ -0,0 +1,18 @@
+def count_game(file_name="game_stat.txt"):
+    pass
+
+
+def decide(file_name="game_stat.txt", year):
+    pass
+
+
+def get_latest(file_name="game_stat.txt"):
+    pass
+
+
+def count_by_genre(file_name="game_stat.txt", genre):
+    pass
+
+
+def get_line_number_by_title(file_name="game_stat.txt", title):
+    pass
