08d92e49c275764895493959164f99856a16e16f
filipHartman <filip.hartman23@gmail.com>
2017-08-29 12:46:58 +0200

    Add count_game function in reports.py

--- reports.py
+++ reports.py
@@ -1,18 +1,28 @@
+import csv
+
+
 def count_game(file_name="game_stat.txt"):
-    pass
+    with open(file_name, "r") as game_stat_file:
+        counter = 0
+        for game in game_stat_file:
+            counter += 1
+    return counter
 
 
-def decide(file_name="game_stat.txt", year):
-    pass
 
 
-def get_latest(file_name="game_stat.txt"):
-    pass
+
+# def get_latest(file_name="game_stat.txt"):
+#     pass
+#
+#
+# def count_by_genre(file_name="game_stat.txt", genre):
+#     pass
+#
+#
+# def get_line_number_by_title(file_name="game_stat.txt", title):
+#     pass
 
 
-def count_by_genre(file_name="game_stat.txt", genre):
-    pass
-
-
-def get_line_number_by_title(file_name="game_stat.txt", title):
-    pass
+print(count_game())
+print(decide(0))
