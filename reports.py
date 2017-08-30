4534b9d6485a9bef0ae07f2ac0ba74f4497228ef
filipHartman <filip.hartman23@gmail.com>
2017-08-30 13:08:58 +0200

    Add get_latest function in reports.py. Changed names of variables from year to date without input

--- reports.py
+++ reports.py
@@ -1,7 +1,7 @@
 import csv
 
 
-def count_game(file_name="game_stat.txt"):
+def count_games(file_name="game_stat.txt"):
     with open(file_name, "r") as game_stat_file:
         counter = 0
         for game in game_stat_file:
@@ -9,29 +9,39 @@ def count_game(file_name="game_stat.txt"):
     return counter
 
 
-# def list_of_games(file_name="game_stat.txt", games_number=count_game()):
-#     with open(file_name, "r") as game_stat_file:
-#         game_list = []
-#         games_file = csv.reader(game_stat_file, delimiter="\t")
-#         for line in games_file:
-#             game_list.append(line)
-#     return game_list
-
-
 def decide(year, file_name="game_stat.txt"):
     with open(file_name, "r") as game_stat_file:
         game_file = csv.reader(game_stat_file, delimiter="\t")
-        years_of_production = []
+        dates_of_production = []
+        date = 2
         for line in game_file:
-            years_of_production.append(line[2])
-        for i in range(count_game()):
-            if year in years_of_production:
+            dates_of_production.append(line[date])
+        for i in range(len(dates_of_production)):
+            if year in dates_of_production:
                 return True
         return False
 
 
-# def get_latest(file_name="game_stat.txt"):
-#     pass
+def get_latest(file_name="game_stat.txt"):
+    with open(file_name, "r") as game_stat_file:
+        game_file = csv.reader(game_stat_file, delimiter="\t")
+
+        title_date_list = []
+        title = 0
+        date = 2
+        for line in game_file:
+            title_date_list.append((line[title], line[date]))
+
+        latest_date = 0
+
+        date = 1
+        position_in_file = 0
+        for i in range(len(title_date_list)):
+            if int(title_date_list[i][date]) > latest_date:
+                latest_date = int(title_date_list[i][date])
+                position_in_file = i
+        return title_date_list[position_in_file][title]
+
 #
 #
 # def count_by_genre(file_name="game_stat.txt", genre):
@@ -43,10 +53,10 @@ def decide(year, file_name="game_stat.txt"):
 
 
 def main():
-    print(count_game())
+    print(count_games())
     # print(list_of_games())
     year = input("What year woluld You like to check: ")
     print(decide(year))
-
+    print(get_latest())
 
 main()
