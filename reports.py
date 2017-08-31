01768b2da113d9eeea2d3cf8e1c69d58b35a6b35
filipHartman <filip.hartman23@gmail.com>
2017-08-31 13:07:12 +0200

    Add 5 first base function in reports.py

--- reports.py
+++ reports.py
@@ -1,62 +1,89 @@
 import csv
 
 
-def count_games(file_name="game_stat.txt"):
+def count_games(file_name):
     with open(file_name, "r") as game_stat_file:
-        counter = 0
+        game_counter = 0
         for game in game_stat_file:
-            counter += 1
-    return counter
+            game_counter += 1
+    return game_counter
 
 
-def decide(year, file_name="game_stat.txt"):
+def decide(file_name, year):
     with open(file_name, "r") as game_stat_file:
         game_file = csv.reader(game_stat_file, delimiter="\t")
         dates_of_production = []
-        date = 2
+        date_column = 2
+        answer = False
         for line in game_file:
-            dates_of_production.append(line[date])
-        for i in range(len(dates_of_production)):
-            if year in dates_of_production:
-                return True
-        return False
+            dates_of_production.append(int(line[date_column]))
+
+        if year in dates_of_production:
+            answer = True
+        return answer
 
 
-def get_latest(file_name="game_stat.txt"):
+def get_latest(file_name):
     with open(file_name, "r") as game_stat_file:
         game_file = csv.reader(game_stat_file, delimiter="\t")
-
         title_date_list = []
-        title = 0
-        date = 2
+        title_column = 0
+        date_column = 2
         for line in game_file:
-            title_date_list.append((line[title], line[date]))
-
+            title_date_list.append((line[title_column], line[date_column]))
         latest_date = 0
-
-        date = 1
+        date_column = 1
         position_in_file = 0
         for i in range(len(title_date_list)):
-            if int(title_date_list[i][date]) > latest_date:
-                latest_date = int(title_date_list[i][date])
+            if int(title_date_list[i][date_column]) > latest_date:
+                latest_date = int(title_date_list[i][date_column])
                 position_in_file = i
-        return title_date_list[position_in_file][title]
+        return title_date_list[position_in_file][title_column]
 
-#
-#
-# def count_by_genre(file_name="game_stat.txt", genre):
-#     pass
-#
-#
-# def get_line_number_by_title(file_name="game_stat.txt", title):
-#     pass
+
+def count_by_genre(file_name, genre):
+    with open(file_name, "r") as game_stat_file:
+        game_file = csv.reader(game_stat_file, delimiter="\t")
+        genre_list = []
+        genre_column = 3
+        for line in game_file:
+            genre_list.append(line[genre_column].upper())
+
+        genre_counter = 0
+        for i in genre_list:
+            if genre.upper() == i:
+                genre_counter += 1
+        return genre_counter
+
+
+def get_line_number_by_title(file_name, title,):
+    with open(file_name, "r") as game_stat_file:
+        game_file = csv.reader(game_stat_file, delimiter="\t")
+        title_dict = {}
+        title_column = 0
+        title_position = 1
+        for line in game_file:
+            title_dict[title_position] = line[title_column]
+            title_position += 1
+        try:
+            for i in title_dict.keys():
+                if title.upper() == title_dict[i]. upper():
+                    return i
+        except ValueError:
+            err_str = "This game is not in the file."
+            return err_str
 
 
 def main():
-    print(count_games())
-    # print(list_of_games())
-    year = input("What year woluld You like to check: ")
-    print(decide(year))
-    print(get_latest())
+    pass
+    # print(count_games())
+    # year = input("What year woluld You like to check: ")
+    # print(decide("game_stat.txt", year))
+    # print(get_latest())
+    # genre = input("Enter the genre to check how many we have: ")
+    # print(count_by_genre(genre))
+    # title = input("Enter the title of the game to check position in the file: ")
+    # print(get_line_number_by_title(title))
+
 
 main()
